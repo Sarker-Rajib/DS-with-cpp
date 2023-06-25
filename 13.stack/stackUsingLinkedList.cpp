746374dfbfae76bd9abcc070ebf8f0046bbd4880
Rajib Sarker <redhat0593@gmail.com>
2023-06-25 23:10:12 +0600

    learned

---
+++ 13.stack/stackUsingLinkedList.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class myListStack
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int sz = 0;
+
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        newNode->prev = tail;
+        tail->next = newNode;
+        tail = tail->next;
+    }
+
+    void pop()
+    {
+        sz--;
+        Node *deletedNode = tail;
+        tail = tail->prev;
+        if (tail == NULL)
+        {
+            head = NULL;
+        }
+        else
+        {
+            tail->next = NULL;
+        }
+
+        delete deletedNode;
+    }
+
+    int top()
+    {
+        return tail->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        if (sz == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    myListStack st;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    // _____
+    return 0;
+}
