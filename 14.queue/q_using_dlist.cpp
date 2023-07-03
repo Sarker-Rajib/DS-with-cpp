ea9a393c6b7fd3965f7a5594529c03c6d3d1e27a
Rajib Sarker <redhat0593@gmail.com>
2023-07-03 23:31:49 +0600

    queue added

---
+++ 14.queue/q_using_dlist.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int val)
+    {
+        sz++;
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = tail->next;
+    }
+
+    void pop()
+    {
+        sz--;
+        Node *deletedNod = head;
+        head = head->next;
+
+        if (head == NULL)
+        {
+            tail == NULL;
+            delete deletedNod;
+            return;
+        }
+
+        delete deletedNod;
+        head->prev = NULL;
+    }
+
+    int front()
+    {
+        return head->val;
+    }
+
+    int size()
+    {
+        return sz;
+    }
+
+    bool empty()
+    {
+        if (sz == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    myQueue q;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+    return 0;
+}
